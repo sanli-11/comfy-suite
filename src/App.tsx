c70b2a506d53b610a0cd2116cec5fdfeeab8cd45
SanLi <sanli.has11@gmail.com>
2024-08-24 13:25:13 +0500

    Created Hero Section
    - Added main to App.tsx
    - Created and added Hero
    - Added Hero texts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="overflow-hidden">
       <Header />
+      <main className="bg-black">
+        <Hero />
+      </main>
     </div>
   )
 }
