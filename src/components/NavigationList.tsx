56d0a6933948d5518c3b747f0e62c9ff77d424ed
SanLi <sanli.has11@gmail.com>
2024-08-25 04:13:04 +0500

    Modified NavigationList to have down-chevron after selected items
    - Added an extra property to index file to indicate which items have
      chevron
    - Modified the NavigationList component to include the chevron-down
      svg after items

--- src/components/NavigationList.tsx
+++ src/components/NavigationList.tsx
@@ -1,16 +1,27 @@
 import { navigationItemsList } from "../constants/header";
+import down from "../assets/svg/chevron-down.svg";
 
 function NavigationList(): JSX.Element {
   return (
     <ul className="flex items-center justify-evenly">
       {navigationItemsList.map(item => (
         <li key={item.id} className="mx-6 font-semibold uppercase text-white">
-          <a href={item.href}>
-            <span>{item.name}</span>
-          </a>
+          {item.haveChevron ?
+            (
+              <a href={item.href} className="flex justify-center items-center">
+                <span>{item.name}</span>
+                <img className="ml-1" src={down} alt="More" width="15" height="15" />
+              </a>
+            ) : (
+              <a href={item.href}>
+                <span>{item.name}</span>
+              </a>
+            )
+          }
         </li>
-      ))}
-    </ul>
+      ))
+      }
+    </ul >
   );
 }
 
--- src/constants/header.ts
+++ src/constants/header.ts
@@ -2,6 +2,7 @@ interface listItems {
 	id: number,
 	name: string,
 	href: string,
+	haveChevron: boolean,
 }
 
 const navigationItemsList: listItems[] = [
@@ -9,29 +10,37 @@ const navigationItemsList: listItems[] = [
 		id: 0,
 		name: "Home",
 		href: "/",
+		haveChevron: false,
 	},
 	{
 		id: 1,
 		name: "About",
 		href: "/about",
-	}, {
+		haveChevron: false,
+	},
+	{
 		id: 2,
 		name: "Rooms",
 		href: "/rooms",
-	}, {
+		haveChevron: true,
+	},
+	{
 		id: 3,
 		name: "Suites",
 		href: "/suites",
+		haveChevron: true,
 	},
 	{
 		id: 4,
 		name: "Blogs",
 		href: "/blogs",
+		haveChevron: true,
 	},
 	{
 		id: 5,
 		name: "Contact",
 		href: "/contact",
+		haveChevron: false,
 	}
 ];
 
