ba327a762a82eb693ac8c08ded4b27b0f41b121a
SanLi <sanli.has11@gmail.com>
2024-08-24 11:19:20 +0500

    Added Logo
    - Added Placeholder Logo (Div with Red BG)
    - Created Logo Component
    - Added Logo in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
+import Logo from "./Logo";
+
 function Header(): JSX.Element {
   return (
     <header className="mt-6 mx-8 flex items-center justify-between">
+      <Logo />
     </header>
   );
 }
---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+function Logo(): JSX.Element {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="m-3 size-12 bg-red-400 rounded-full" />
+      <div className="flex flex-col items-start justify-start text-white">
+        <h1 className="text-xl font-bold leading-5">Comfy Suite</h1>
+        <h2 className="uppercase font-light text-xs">Hotel Booking</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Logo;
