a82d54819b94eb6f2013ef73e2fdbcde7f25b193
SanLi <sanli.has11@gmail.com>
2024-08-30 06:20:13 +0500

    Added Hero Background Image
    - Added Background Image 'hero' in assets/img
    - Added 'hero' to Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,12 @@
+import HeroBG from "../assets/img/hero.jpg";
 import Button from "./Button";
-import HeroButtons from "./HeroButtons";
 import HeroFloatingButton from "./HeroFloatingButton";
 
 function Hero(): JSX.Element {
   return (
-    <section className="w-full h-dvh flex items-center justify-center bg-black">
+    <section className="w-full h-dvh flex items-center justify-center bg-bottom bg-cover" style={{
+      backgroundImage: `url(${HeroBG})`
+    }}>
       <div className="w-2/5 text-center uppercase text-white">
         <h2 className="pb-4 text-xl font-medium tracking-wider">Elevate your Travel</h2>
         <pre className="text-[2.75rem] font-semibold leading-normal whitespace-pre-wrap">
@@ -13,9 +15,8 @@ function Hero(): JSX.Element {
         </pre>
         <Button className="mt-8 border-orange-400 bg-orange-400">Book Hotel Now</Button>
       </div>
-      <HeroButtons />
       <HeroFloatingButton />
-    </section>
+    </section >
   );
 }
 
